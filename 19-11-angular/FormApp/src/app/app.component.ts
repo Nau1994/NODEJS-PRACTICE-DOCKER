e246d5e174269a381fb0ac9d4ca6afc128905478
Naushad <naushad.a1013@gmail.com>
2023-11-27 12:47:13 +0530

    Reactive Form

--- 19-11-angular/FormApp/src/app/app.component.ts
+++ 19-11-angular/FormApp/src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // standalone:true,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'FormApp';
+  user:any;
+  getData(data:any){
+    console.log(data)
+  }
 }
